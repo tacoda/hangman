ade9f54477cf8bb9c251bc3034cc05701eaa2d88
Ian Johnson <ijohnson@covermymeds.com>
2019-05-25 12:06:24 -0400

    Complete migration to GenServer

--- lib/hangman/game.ex
+++ lib/hangman/game.ex
@@ -51,9 +51,9 @@ defmodule Hangman.Game do
   end
 
   defp reveal_letter(letter, _in_word = true), do: letter
-  defp reveal_letter(letter, _not_in_word), do: "_"
+  defp reveal_letter(_letter, _not_in_word), do: "_"
 
-  defp accept_move(game, guess, _already_guessed = true) do
+  defp accept_move(game, _guess, _already_guessed = true) do
     Map.put(game, :game_state, :already_used)
   end
 
