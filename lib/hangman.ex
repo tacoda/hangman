df9720af25eac30d0602d6748f9eff4b776beceb
Ian Johnson <ijohnson@covermymeds.com>
2019-05-25 11:29:57 -0400

    Basic program implemented

--- lib/hangman.ex
+++ lib/hangman.ex
@@ -4,11 +4,6 @@ defmodule Hangman do
 
   defdelegate new_game(),             to: Game
   defdelegate tally(game),            to: Game
+  defdelegate make_move(game, guess), to: Game
 
-  def make_move(game, guess) do
-    game = Game.make_move(game, guess)
-    { game, tally(game) }
-  end
-
-  # I’m still unsure about writing that function in hangman.ex. Another way to achieve the same effect is to rename make_move in game.ex to something else, and then write a new make_move that calls this function, and then returns it and the tally. That way, the API module would be just three defdelegate calls. Which do you prefer?
 end
--- test/game_test.exs
+++ test/game_test.exs
@@ -19,33 +19,33 @@ defmodule GameTest do
 
   test "state isn't changed for :won game" do
     game = Game.new_game() |> Map.put(:game_state, :won)
-    assert ^game = Game.make_move(game, "x")
+    assert {^game, _tally} = Game.make_move(game, "x")
   end
 
   test "state isn't changed for :won or :lost game" do
     for state <- [ :won, :lost ] do
       game = Game.new_game() |> Map.put(:game_state, state)
-      assert ^game = Game.make_move(game, "x")
+      assert {^game, _tally} = Game.make_move(game, "x")
     end
   end
 
   test "first occurrence of letter is not already used" do
     game = Game.new_game()
-    game = Game.make_move(game, "x")
+    {game, _tally} = Game.make_move(game, "x")
     assert game.game_state != :already_used
   end
 
   test "second occurrence of letter is already used" do
     game = Game.new_game()
-    game = Game.make_move(game, "x")
+    {game, _tally} = Game.make_move(game, "x")
     assert game.game_state != :already_used
-    game = Game.make_move(game, "x")
+    {game, _tally} = Game.make_move(game, "x")
     assert game.game_state == :already_used
   end
 
   test "a good guess is recognized" do
     game = Game.new_game("wibble")
-    game = Game.make_move(game, "w")
+    {game, _tally} = Game.make_move(game, "w")
     assert game.game_state == :good_guess
     assert game.turns_left == 7
   end
@@ -81,7 +81,7 @@ defmodule GameTest do
     game = Game.new_game("wibble")
 
     Enum.reduce(moves, game, fn({ guess, state }, game) ->
-      game = Game.make_move(game, guess)
+      {game, _tally} = Game.make_move(game, guess)
       assert game.game_state == state
       game
     end)
@@ -89,32 +89,32 @@ defmodule GameTest do
 
   test "bad guess is recognized" do
     game = Game.new_game("wibble")
-    game = Game.make_move(game, "x")
+    {game, _tally} = Game.make_move(game, "x")
     assert game.game_state == :bad_guess
     assert game.turns_left == 6
   end
 
   test "lost game is recognized" do
     game = Game.new_game("w")
-    game = Game.make_move(game, "a")
+    {game, _tally} = Game.make_move(game, "a")
     assert game.game_state == :bad_guess
     assert game.turns_left == 6
-    game = Game.make_move(game, "b")
+    {game, _tally} = Game.make_move(game, "b")
     assert game.game_state == :bad_guess
     assert game.turns_left == 5
-    game = Game.make_move(game, "c")
+    {game, _tally} = Game.make_move(game, "c")
     assert game.game_state == :bad_guess
     assert game.turns_left == 4
-    game = Game.make_move(game, "d")
+    {game, _tally} = Game.make_move(game, "d")
     assert game.game_state == :bad_guess
     assert game.turns_left == 3
-    game = Game.make_move(game, "e")
+    {game, _tally} = Game.make_move(game, "e")
     assert game.game_state == :bad_guess
     assert game.turns_left == 2
-    game = Game.make_move(game, "f")
+    {game, _tally} = Game.make_move(game, "f")
     assert game.game_state == :bad_guess
     assert game.turns_left == 1
-    game = Game.make_move(game, "g")
+    {game, _tally} = Game.make_move(game, "g")
     assert game.game_state == :lost
   end
 
